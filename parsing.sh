11153ed6fe74f1fc5e94265a83a1ec7af20b4c01
rsan <dystraktor@gmail.com>
2019-02-06 01:34:18 -0600

    se agregan dos trucos mas para el json de masscan

--- parsing.sh
+++ parsing.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-parsing_masscan_json(){
+parsing_masscan_json_to_nmap(){
 cat "$1" | jq -r '.[] | "nmap -sV -sS -T4 -n -Pn --open -oX nmap_"+ .ip + ".xml -p" + ([.ports[].port | tostring] | join(" ")) + " " + .ip'
 }
 
+parsing_masscan_list_host(){
+cat "$1" |  jq '.[]| .ip' | sort -u;
+}
+
+parsing_masscan_list_ports(){
+cat "$1"| jq '.[].ports[] | select(.status == "open") | .port' | sort -u -n
+}
