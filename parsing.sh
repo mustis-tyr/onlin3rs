1392be4c8d97b6609d8c72f1d1acf7f27201f0c8
rsan <dystraktor@gmail.com>
2019-02-06 02:07:42 -0600

    cambios para poder arreglar el archivo que arroja masscan

--- parsing.sh
+++ parsing.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+fix_masscan_json(){
+echo "[" "$(cat $1  | grep -v 'finished')" "{}]" | jq '.[] | select(.ip != null)'
+}
+
 parsing_masscan_json_to_nmap(){
 cat "$1" | jq -r '.[] | "nmap -sV -sS -T4 -n -Pn --open -oX nmap_"+ .ip + ".xml -p" + ([.ports[].port | tostring] | join(" ")) + " " + .ip'
 }
 
 parsing_masscan_list_host(){
-cat "$1" |  jq '.[]| .ip' | sort -u;
+cat "$1" |  jq '.[]| .ip' | sort -u | tr -d "\"";
 }
 
 parsing_masscan_list_ports(){
 cat "$1"| jq '.[].ports[] | select(.status == "open") | .port' | sort -u -n
+}
+
+analize_schema(){
+cat "$1" | jq -r 'path(..) | map(tostring) | join("/")';
 }
