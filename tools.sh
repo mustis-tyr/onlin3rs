fd1f6c0c0d2aa8c347354c116206eca71d52aea9
rsan <dystraktor@gmail.com>
2019-02-07 00:36:16 -0600

    aun me falta definir como lanzar un commando de nmap y masscan para poder escanear esos puertos mas comunes

--- tools.sh
+++ tools.sh
@@ -5,3 +5,9 @@ echo -n "powershell -nOp -w hIDdeN -EP ByPaSs -e ";
 echo $(cat "$1" | iconv -t UTF-16LE | base64 | tr -d "\n"); 
 }
 
+common-attacked-ports(){
+PORTS="2222 22 445 502 80 23 22321 41967 1433 25 8545 3389 8080 37215 443 5060 81 5900 2323 8088"
+
+
+}
+
